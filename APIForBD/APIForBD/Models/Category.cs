3fd81db336e5e2229c7c4e33dbdaf2dff0d50809
Ares25123 <milutin25a@gmail.com>
2024-01-23 12:10:51 +0300

    api

---
+++ APIForBD/APIForBD/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIForBD.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public int CreatedBy { get; set; }
+        public int? DeletedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ APIForBD/APIForBD/Models/Meal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIForBD.Models
+{
+    public partial class Meal
+    {
+        public int MealId { get; set; }
+        public string MealName { get; set; } = null!;
+        public string MealDescription { get; set; } = null!;
+        public TimeSpan PreparatonTime { get; set; }
+        public double Kkal { get; set; }
+        public int UserId { get; set; }
+        public int CreatedBy { get; set; }
+        public int? DeletedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public virtual User User { get; set; } = null!;
+    }
+}
