c221ab40bfdc44f96c13326e961c3ca7bdad34d2
Ares25123 <milutin25a@gmail.com>
2024-01-23 12:10:22 +0300

    s

--- src/APIForBD/APIForBD/Contracts/Category/CreateCategoryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIForBD.Contracts.Category
-{
-    public class CreateCategoryRequest
-    {
-        public string CategoryName { get; set; } = null!;
-        public int CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
-}
